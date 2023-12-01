4f9afdcf459ab4e393e4c1b493aa675f6140bae0
Shuainan Yang <shannon_yang@163.com>
2023-12-01 03:07:12 -0500

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,5 +9,10 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
+// ...
 module.exports = router;
